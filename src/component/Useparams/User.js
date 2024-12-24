b0862167a373c616748d5a1d26d22b6882a45b77
satishjamle <satishjamle513@gmail.com>
2024-12-24 17:31:58 +0530

    uselocation and usenavigate hooks(usehistory)

--- src/component/Useparams/User.js
+++ src/component/Useparams/User.js
@@ -1,20 +1,64 @@
+// import React from 'react'
+// import { useParams} from 'react-router-dom'
+
+// export const User = () => {
+
+//     const {fname ,lname} = useParams()
+//   return (
+//     <>
+//     <h1>my name is   {fname}  <br /> {lname} </h1>
+//     </>
+//   )
+// }
+
+// use location  hooks----------------------------
+
+// import React from "react";
+// import { useLocation, useParams } from "react-router-dom";
+
+// export const User = () => {
+//   const location = useLocation();
+//   console.log(location);
+
+//   const { fname, lname } = useParams();
+//   return (
+//     <>
+//       <h1>
+//         my name is {fname} <br /> {lname}{" "}
+//       </h1>
+//       <p>my current location is {location.pathname}</p>
+//       {location.pathname === `/User/satish/Developer` ? (
+//         <button onClick={()=>alert('you are osm')}> click me</button>
+//       ) : null}
+//     </>
+//   );
+// };
+
+// use history hooks--------------------
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams, useNavigate } from "react-router-dom"; // Import useNavigate
 
 export const User = () => {
   const location = useLocation();
   console.log(location);
 
   const { fname, lname } = useParams();
+  const navigate = useNavigate(); // Use useNavigate instead of useHistory
+
   return (
-    <>
-      <h1>
-        my name is {fname} <br /> {lname}{" "}
+    <div className="user-container">
+      <h1 className="user-heading">
+        My name is {fname} <br /> {lname}
       </h1>
-      <p>my current location is {location.pathname}</p>
+      <p className="user-location">My current location is {location.pathname}</p>
       {location.pathname === `/User/satish/Developer` ? (
-        <button onClick={()=>alert('you are osm')}> click me</button>
+        <button onClick={() => {
+          alert('You are awesome!');
+          navigate('/another-page'); // Example of navigation with useNavigate
+        }}>
+          Click Me
+        </button>
       ) : null}
-    </>
+    </div>
   );
 };
