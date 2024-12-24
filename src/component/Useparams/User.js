9024c032701bdd60e438bf3986ab1a4f16aff79b
satishjamle <satishjamle513@gmail.com>
2024-12-24 15:56:10 +0530

    uselocation hooks

--- src/component/Useparams/User.js
+++ src/component/Useparams/User.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { useParams} from 'react-router-dom'
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
 
 export const User = () => {
+  const location = useLocation();
+  console.log(location);
 
-    const {fname ,lname} = useParams()
+  const { fname, lname } = useParams();
   return (
     <>
-    <h1>my name is   {fname}  <br /> {lname} </h1>
+      <h1>
+        my name is {fname} <br /> {lname}{" "}
+      </h1>
+      <p>my current location is {location.pathname}</p>
+      {location.pathname === `/User/satish/Developer` ? (
+        <button onClick={()=>alert('you are osm')}> click me</button>
+      ) : null}
     </>
-  )
-}
-
-
+  );
+};
